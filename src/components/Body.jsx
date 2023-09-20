1da6cb5df49797dadad91dcdcc594f03112b35e1
vijay <digitalxvijay@gmail.com>
2023-09-20 22:51:12 +0530

    Ep-6 Lets explore the world

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,58 +1,50 @@
 import { useEffect, useState } from "react";
-import resList from "../utils/mockData";
+// import resList from "../utils/mockData";
 import RestuarantCard from "./RestaurantCard";
 
 const Body = () => {
-  const [listOfRestuarants, setlistOfRestuarant] = useState(resList);
+  const [listOfRestuarants, setlistOfRestuarant] = useState([]);
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
     const data = await fetch(
-      'https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING'
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
-    console.log(json);
+    console.log(
+      json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+    );
     setlistOfRestuarant(
       // json.data.cards[2].data.data.cards
-      json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants
+      json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
     );
   };
-  // async function getRestaurants() {
-  //   try {
-  //   const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-  //   const json = await data.json()
-  //     console.log(json)
-  //     setRestaurantList(json?.data?.cards[2]?.card?.card.gridElements.infoWithStyle.restaurants);
-  //     filterSetRestaurantList(json?.data?.cards[2]?.card?.card.gridElements.infoWithStyle.restaurants);
-  //     } catch (error) {
-  //     console.log("some error has occured " + error)
-  //   }
 
-  // }
-
-  return (
-    <div className="body">
-      <div className="filter">
+  return listOfRestuarants.length > 0 ? (
+    <div className='body'>
+      <div className='filter'>
         <button
-          className="filter-btn"
+          className='filter-btn'
           onClick={() => {
-            const filteredList = (listOfRestuarants = listOfRestuarants.filter(
-              (res) => res.data.avgRating > 4
-            ));
+            let filteredList =  listOfRestuarants.filter(
+              (res) => res.info.avgRating > 4
+            );
             setlistOfRestuarant(filteredList);
           }}
         >
           Top Rated
         </button>
       </div>
-      <div className="res-container">
-        {listOfRestuarants.map((restaurant, index) => (
-          <RestuarantCard key={index} resData={restaurant} />
+      <div className='res-container'>
+        {listOfRestuarants.map((restaurant) => (
+          <RestuarantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
+  ) : (
+    <h1>Loading...</h1>
   );
 };
 
--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,22 +1,16 @@
-import {CDN_URL} from "../utils/constants";
+import { CDN_URL } from "../utils/constants";
 const RestuarantCard = (props) => {
   const { resData } = props;
-  const {
-    name,
-    cuisines,
-    avgRating,
-    costForTwo,
-    deliveryTime,
-    cloudinaryImageId,
-  } = resData?.data;
+  const { name, cuisines, avgRating, costForTwo, sla, cloudinaryImageId } =
+    resData?.info;
   return (
-    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
-      <img className="res-logo" src={CDN_URL + cloudinaryImageId} />
+    <div className='res-card' style={{ backgroundColor: "#f0f0f0" }}>
+      <img className='res-logo' src={CDN_URL + cloudinaryImageId} />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating}</h4>
       <h4>{costForTwo / 100}</h4>
-      <h4>{deliveryTime}</h4>
+      <h4>{sla.deliveryTime}</h4>
     </div>
   );
 };
