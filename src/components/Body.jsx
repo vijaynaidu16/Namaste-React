cbfa849d524d42aaf226d893f4e9ab2e53df4e8c
vijay <digitalxvijay@gmail.com>
2024-01-26 22:14:56 +0530

    Ep 11 - Data is the new Oil

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,37 +3,45 @@ import RestaurantCard, { withPromotedLabel } from "./RestaurantCard";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
-// import data from './../utils/mockData';
+import axios from "axios";
 
 const Body = () => {
   const [listOfRestuarants, setlistOfRestuarant] = useState([]);
   const [filteredRestaurant, setfilteredRestaurant] = useState([]);
   const [searchText, setsearchText] = useState("");
   const RestaurantCardPromoted = withPromotedLabel(RestaurantCard);
-  console.log(listOfRestuarants);
+  // console.log(listOfRestuarants);
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch(
-      // "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.385044&lng=78.486671&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.385044&lng=78.486671&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-    const json = await data.json();
-    setlistOfRestuarant(
-      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-    setfilteredRestaurant(
-      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
+    try {
+      const data = await fetch(
+        "https://api.allorigins.win/raw?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D15.8323892%26lng%3D78.0544946%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING"
+      );
+      const json = await data.json();
+      console.log(json);
+      // Optional Chaining
+      setlistOfRestuarant(
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+      setfilteredRestaurant(
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   };
+
   const onlineStatus = useOnlineStatus();
   if (onlineStatus === false) {
     return <h1>You are offline!! Check your internet Connection</h1>;
   }
 
-  return listOfRestuarants.length > 0 ? (
+  return listOfRestuarants || [].length > 0 ? (
     <div className="body">
       <div className="filter flex ">
         <div className="search m-4 p-4">
@@ -73,15 +81,14 @@ const Body = () => {
         </div>
       </div>
       <div className="flex flex-wrap ">
-        {filteredRestaurant.map((restaurant) => (
+        {filteredRestaurant?.map((restaurant) => (
           <Link
-          key={restaurant.info.id}
-          to={"/restaurants/" + restaurant.info.id}
+            key={restaurant.info.id}
+            to={"/restaurants/" + restaurant.info.id}
           >
             {restaurant?.info.promoted ? (
               <RestaurantCardPromoted resData={restaurant} />
             ) : (
-              
               <RestaurantCard resData={restaurant} />
             )}
           </Link>
@@ -95,8 +102,3 @@ const Body = () => {
 
 // https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING
 export default Body;
-// {restaurant?.info.promoted ? (
-//   <RestaurantCard resData={restaurant} />
-// ) : (
-//   <RestaurantCardPromoted resData={restaurant} />
-// )}
--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -2,8 +2,7 @@ export const LOGO_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQQ9W98Mg2_uVUCWTJY_WkT0adfjOEXCvHRQ";
 export const CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-export const MENU_API = 
-"https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
-// "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.385044&lng=78.486671&restaurantId=";
-// "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.385044&lng=78.486671&restaurantId=";
-// https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.385044&lng=78.486671&restaurantId=387147#
+  export const MENU_API =
+  "https://api.allorigins.win/raw?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D15.8323892%26lng%3D78.0544946%26restaurantId%3D"
+  // "https://api.allorigins.win/raw?url=https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Fmenu%2Fpl%3Fpage-type%3DREGULAR_MENU%26complete-menu%3Dtrue%26lat%3D15.8323892%26lng%3D78.0544946%26restaurantId%3D"
+  // https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
